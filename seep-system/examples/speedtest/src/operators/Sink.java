a595d15a1c0969e5bc22ec86e1beb5efed825cfc
Nicola Toscan <nicolatoscan99@outlook.it>
2023-02-06 04:13:51 +0100

    speedtest time

--- seep-system/examples/speedtest/src/operators/Sink.java
+++ seep-system/examples/speedtest/src/operators/Sink.java
@@ -1,5 +1,6 @@
 package operators;
 
+import java.util.Date;
 import java.util.List;
 import uk.ac.imperial.lsds.seep.comm.serialization.DataTuple;
 import uk.ac.imperial.lsds.seep.operator.StatelessOperator;
@@ -8,14 +9,18 @@ public class Sink implements StatelessOperator {
 
 	private static final long serialVersionUID = 1L;
 	private int tupleReceived = 0;
+	private long lastDate = new Date().getTime();
 	
 	public void setUp() { }
 
 	public void processData(DataTuple dt) {
 		int value1 = dt.getInt("value1");
 		tupleReceived++;
+
 		if(tupleReceived % 1000 == 0){
-			System.out.println("PY,SINK," + tupleReceived + "," + value1);
+			long now = new Date().getTime();
+			System.out.println("PY,SINK,TupleReceived=" + tupleReceived + ",value=" + value1 + ",time=" + (now - this.lastDate));
+			this.lastDate = now;
 		}
 	}
 	
